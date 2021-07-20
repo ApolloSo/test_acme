d78866a419ac7b233aeb6a10b502ace66a43c0cb
osboxes.org <euxnad@outlook.com>
2021-07-20 08:40:18 -0400

    datatable

--- test_acme_front/src/components/AcmeTable.vue
+++ test_acme_front/src/components/AcmeTable.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>Acme table </div>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="data"
+      :items-per-page="5"
+    >
+    </v-data-table>
+  </v-container>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 @Component
 export default class AcmeTable extends Vue {
-  @Prop() private data!: Array;
+  @Prop() private data!: Array<any>;
+  headers: Array<any> = [
+    {
+      text: 'Dessert (100g serving)',
+      align: 'start',
+      sortable: false,
+      value: 'name',
+    },
+    { text: 'Calories', value: 'calories' },
+    { text: 'Fat (g)', value: 'fat' },
+    { text: 'Carbs (g)', value: 'carbs' },
+    { text: 'Protein (g)', value: 'protein' },
+    { text: 'Iron (%)', value: 'iron' },
+  ]
 }
 </script>
