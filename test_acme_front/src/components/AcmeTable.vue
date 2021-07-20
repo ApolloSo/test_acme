3e40bf2fad49c2ad63f61f3e9b1dc2b71f521271
osboxes.org <euxnad@outlook.com>
2021-07-20 13:46:29 -0400

    updated

--- test_acme_front/src/components/AcmeTable.vue
+++ test_acme_front/src/components/AcmeTable.vue
@@ -1,29 +1,26 @@
 <template>
-  <v-container>
-    <v-data-table
-      :headers="headers"
-      :items="data"
-      :items-per-page="5"
-    >
+  <v-container fluid>
+    <v-data-table :headers="headers" :items="data" :items-per-page="5">
     </v-data-table>
   </v-container>
 </template>
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from "vue-property-decorator";
+
 @Component
 export default class AcmeTable extends Vue {
   public headers: any[] = [
     {
-      text: 'Dessert (100g serving)',
-      align: 'start',
+      text: "Dessert (100g serving)",
+      align: "start",
       sortable: false,
-      value: 'name',
+      value: "name",
     },
-    { text: 'Calories', value: 'calories' },
-    { text: 'Fat (g)', value: 'fat' },
-    { text: 'Carbs (g)', value: 'carbs' },
-    { text: 'Protein (g)', value: 'protein' },
-    { text: 'Iron (%)', value: 'iron' },
+    { text: "Calories", value: "calories" },
+    { text: "Fat (g)", value: "fat" },
+    { text: "Carbs (g)", value: "carbs" },
+    { text: "Protein (g)", value: "protein" },
+    { text: "Iron (%)", value: "iron" },
   ];
   @Prop() private data!: any[];
 }
