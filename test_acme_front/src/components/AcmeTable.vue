bd0d7e22f5f65bbf9a5271fefc68ff95e378eb53
osboxes.org <euxnad@outlook.com>
2021-07-20 18:09:52 -0400

    updated

--- test_acme_front/src/components/AcmeTable.vue
+++ test_acme_front/src/components/AcmeTable.vue
@@ -1,27 +1,59 @@
 <template>
   <v-container fluid>
-    <v-data-table :headers="headers" :items="data" :items-per-page="5">
+    <v-data-table :headers="headers" :items="data">
+      <template v-slot:item.name="{ item }">
+        <div class="d-flex">
+          <span v-if="itemToEdit != item.id">{{ item.name }}</span>
+          <v-text-field
+            @keyup.enter="submit(item.id, item.name)"
+            v-if="itemToEdit == item.id"
+            v-model="item.name"
+            dense
+          ></v-text-field>
+          <v-icon
+            v-if="itemToEdit != item.id"
+            @click="itemToEdit = item.id"
+            small
+            >mdi-lead-pencil</v-icon
+          >
+        </div>
+      </template>
+      <template v-slot:item.description="{ item }">
+        {{ item.description.slice(0, 30) }}
+      </template>
     </v-data-table>
   </v-container>
 </template>
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
-
-@Component
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { mapActions } from 'vuex';
+@Component({
+  methods: {
+    ...mapActions(['getColStores']),
+  },
+})
 export default class AcmeTable extends Vue {
-  public headers: any[] = [
+  private headers: any[] = [
+    { text: 'ID', value: 'id' },
     {
-      text: "Dessert (100g serving)",
-      align: "start",
+      text: 'Name',
+      align: 'start',
       sortable: false,
-      value: "name",
+      value: 'name',
+      width: '200',
     },
-    { text: "Calories", value: "calories" },
-    { text: "Fat (g)", value: "fat" },
-    { text: "Carbs (g)", value: "carbs" },
-    { text: "Protein (g)", value: "protein" },
-    { text: "Iron (%)", value: "iron" },
+    { text: 'Code', value: 'code' },
+    { text: 'Opening date', value: 'opening_date' },
+    { text: 'Store type', value: 'store_type' },
+    { text: 'Season', value: 'season' },
+    { text: 'Special f1', value: 'special_f1' },
+    { text: 'Special f2', value: 'special_f2' },
+    { text: 'Description', value: 'description' },
   ];
+  private itemToEdit: string = '';
   @Prop() private data!: any[];
+  public submit(id: string, name: string): void {
+    this.itemToEdit = '';
+  }
 }
 </script>
--- test_acme_front/src/store/index.ts
+++ test_acme_front/src/store/index.ts
@@ -1,14 +1,26 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+import { StoreService } from '@/apis';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    colStores: [],
+  },
+  getters: {
+    colStores: (state) => state.colStores,
   },
   mutations: {
+    getColStores(state, payload) {
+      state.colStores = payload;
+    },
   },
   actions: {
+    getColStores({ commit }) {
+      StoreService.getColStores().then((res) => {
+        commit('getColStores', res.data);
+      });
+    },
   },
   modules: {
   },
