07aff5e6b1c86856016a06803253d4f5e402fa7c
osboxes.org <euxnad@outlook.com>
2021-07-21 03:50:39 -0400

    updated

--- test_acme_front/src/config/endpoints.ts
+++ test_acme_front/src/config/endpoints.ts
@@ -3,5 +3,6 @@ const endpoints = {
   API_URL: baseUrl,
   GET_COLSTORES_URL: `${baseUrl}/api/collection_stores`,
   UPDATE_NAME_URL: `${baseUrl}/api/stores/update`,
+  GET_COUNTS_URL: `${baseUrl}/api/collection_stores/getCount`,
 };
 export default endpoints;
